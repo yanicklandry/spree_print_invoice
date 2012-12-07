661b28f1c6916f65f4d39caeeef2e475c8def01a
Yanick Landry <yanick.landry@gmail.com>
2012-12-06 23:43:57 -0500

    allow non-admin users to print invoice

--- app/controllers/spree/orders_controller_decorator.rb
+++ app/controllers/spree/orders_controller_decorator.rb
@@ -1,8 +1,8 @@
-Spree::Admin::OrdersController.class_eval do
+Spree::OrdersController.class_eval do
   respond_to :pdf
 
   def show
-    load_order
+    @order = Spree::Order.find_by_number!(params[:id])
     respond_with(@order) do |format|
       format.pdf do
         template = params[:template] || "invoice"
